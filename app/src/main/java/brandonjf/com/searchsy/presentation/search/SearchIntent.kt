1dc76fccba91c6567d56e27dfc2c3168b29429c5
Brandon John-Freso <brandonjf@gmail.com>
2018-03-14 00:27:47 -0400

    Adding search specific classes

--- app/src/main/java/brandonjf/com/searchsy/presentation/search/SearchIntent.kt
+++ app/src/main/java/brandonjf/com/searchsy/presentation/search/SearchIntent.kt
@@ -1,6 +1,6 @@
 package brandonjf.com.searchsy.presentation.search
 
-import brandonjf.com.searchsy.presentation.base.BaseIntent
+import brandonjf.com.searchsy.base.BaseIntent
 
 /**
  * Created by Brandon on 3/11/18.
@@ -9,4 +9,4 @@ sealed class SearchIntent : BaseIntent {
     object InitialIntent: SearchIntent()
     object LoadListingsIntent: SearchIntent()
     object RefreshListingsIntent: SearchIntent()
-}
+}   
--- app/src/main/java/brandonjf/com/searchsy/presentation/search/view/SearchActivity.kt
+++ app/src/main/java/brandonjf/com/searchsy/presentation/search/view/SearchActivity.kt
@@ -1,13 +1,18 @@
-package brandonjf.com.searchsy
+package brandonjf.com.searchsy.presentation.search.view
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import brandonjf.com.searchsy.R
+import brandonjf.com.searchsy.base.BaseView
+import brandonjf.com.searchsy.base.BaseViewState
+import brandonjf.com.searchsy.presentation.search.SearchIntent
 import brandonjf.com.searchsy.presentation.search.SearchViewModel
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_search.*
 import timber.log.Timber
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), BaseView<SearchIntent, BaseViewState> {
 
     private val viewModel: SearchViewModel by lazy(LazyThreadSafetyMode.NONE) {
         ViewModelProviders.of(this)
@@ -22,6 +27,14 @@ class SearchActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         setupListeners()
+    }
+
+    override fun intents(): Observable<SearchIntent> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun render(state: BaseViewState) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     fun setupListeners() {
